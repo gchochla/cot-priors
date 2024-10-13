1af6bce8f259a42790da81355351a07e8be9443d
Georgios <georgioschochlakis@gmail.com>
2024-10-13 13:58:56 -0700

    Remove hardcoded paths

--- scripts/goemotions.sh
+++ scripts/goemotions.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-while getopts d: flag
+while getopts d:c: flag
 do
     case "${flag}" in
         d) data=${OPTARG};;
+        c) cache_dir=${OPTARG};;
     esac
 done
 
@@ -14,7 +15,7 @@ python scripts/llm_prompting_clsf.py GoEmotions --root-dir $data \
     --system ' ' --instruction $'Classify the following inputs into none, one, or multiple the following emotions per input: {labels}.\n' \
     --incontext $'Input: {text}\nEmotion(s): {label}\n' \
     --shot 5 25 45 --model-name meta-llama/Meta-Llama-3-8B-Instruct meta-llama/Llama-2-7b-chat-hf \
-    --cache-dir /project/shrikann_35/llm-shared --train-keep-same-examples --test-keep-same-examples \
+    --cache-dir $cache_dir --train-keep-same-examples --test-keep-same-examples \
     --train-debug-ann 3 --max-new-tokens 10 --seed 0 1 2 --test-debug-len 100 \
     --keep-one-after-filtering true --label-mode _None_ distribution \
     --load-in-4bit --device cuda:0 --logging-level debug \
@@ -27,7 +28,7 @@ python scripts/prompting/llm_prompting_clsf.py GoEmotions --root-dir $data \
     --system ' ' --instruction $'Classify the following inputs into none, one, or multiple the following emotions per input: {labels}.\n' \
     --incontext $'Input: {text}\n\nReasoning: {cot}\n\nEmotion(s): {label}\n\n' \
     --shot 5 15 --model-name meta-llama/Meta-Llama-3-8B-Instruct meta-llama/Llama-2-7b-chat-hf \
-    --cache-dir /project/shrikann_35/llm-shared --train-keep-same-examples --test-keep-same-examples \
+    --cache-dir $cache_dir --train-keep-same-examples --test-keep-same-examples \
     --train-debug-ann 3 --max-new-tokens 80 --seed 0 1 2 --test-debug-len 100 --keep-one-after-filtering true \
     --cot-csv ./files/goemotions_cot.csv --load-in-4bit --accelerate --logging-level debug \
     --alternative {model_name_or_path}-cot-annotator-{keep_one_after_filtering}-{label_mode}-{shot}-shot
@@ -40,7 +41,7 @@ python scripts/prompting/llm_prompting_clsf.py GoEmotions --root-dir $data \
     --system ' ' --instruction $'Classify the following inputs into none, one, or multiple the following emotions per input: {labels}.\n' \
     --incontext $'Input: {text}\n\nReasoning: {cot}\n\nEmotion(s): {label}\n\n' \
     --shot 5 15 --model-name meta-llama/Meta-Llama-3-8B-Instruct meta-llama/Llama-2-7b-chat-hf \
-    --cache-dir /project/shrikann_35/llm-shared --train-keep-same-examples --test-keep-same-examples \
+    --cache-dir $cache_dir --train-keep-same-examples --test-keep-same-examples \
     --train-debug-ann 3 --max-new-tokens 80 --seed 0 1 2 --test-debug-len 100 \
     --keep-one-after-filtering true --cot-csv ./files/goemotions_cot.csv \
     { --cot-randomize false --label-mode distribution } { --cot-randomize true --label-mode _None_ distribution } \
